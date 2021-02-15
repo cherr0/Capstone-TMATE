700f48809cacd9d966e78b4e23ea3ddc80b71f34
hansu6612 <76238366+hansu6612@users.noreply.github.com>
2021-02-15 20:21:53 +0900

    0215 종합
    사이드바, 네비바, 모달창, 메인

---
+++ demo/src/test/java/com/tmate/demo/TmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
