0a9610d73747b9a256ce96c534dc2be9f2a5af15
kimjinsoo <wlstn571666@nate.com>
2021-02-19 17:16:12 +0900

    오류나서 파일다시받고 인트로페이지 구성했음

---
+++ src/test/java/com/tmate/TmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
