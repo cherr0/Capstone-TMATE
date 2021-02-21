b4d09e4cebc55a97a1e05daffd7c170557e3f614
cherr0 <adsl1664@gmail.com>
2021-02-21 23:16:48 +0900

    차트 관련 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,75 +4,78 @@
   "use strict";
 
   try {
-    //WidgetChart 1
-    // var ctx = document.getElementById("widgetChart1");
-    // if (ctx) {
-    //   ctx.height = 130;
-    //   var myChart = new Chart(ctx, {
-    //     type: 'line',
-    //     data: {
-    //       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    //       type: 'line',
-    //       datasets: [{
-    //         data: [78, 81, 80, 45, 34, 12, 40],
-    //         label: 'Dataset',
-    //         backgroundColor: 'rgba(255,255,255,.1)',
-    //         borderColor: 'rgba(255,255,255,.55)',
-    //       },]
-    //     },
-    //     options: {
-    //       maintainAspectRatio: true,
-    //       legend: {
-    //         display: false
-    //       },
-    //       layout: {
-    //         padding: {
-    //           left: 0,
-    //           right: 0,
-    //           top: 0,
-    //           bottom: 0
-    //         }
-    //       },
-    //       responsive: true,
-    //       scales: {
-    //         xAxes: [{
-    //           gridLines: {
-    //             color: 'transparent',
-    //             zeroLineColor: 'transparent'
-    //           },
-    //           ticks: {
-    //             fontSize: 2,
-    //             fontColor: 'transparent'
-    //           }
-    //         }],
-    //         yAxes: [{
-    //           display: false,
-    //           ticks: {
-    //             display: false,
-    //           }
-    //         }]
-    //       },
-    //       title: {
-    //         display: false,
-    //       },
-    //       elements: {
-    //         line: {
-    //           borderWidth: 0
-    //         },
-    //         point: {
-    //           radius: 0,
-    //           hitRadius: 10,
-    //           hoverRadius: 4
-    //         }
-    //       }
-    //     }
-    //   });
-    // }
+    // WidgetChart 1
+    var ctx = document.getElementById("widgetChart1");
+    if (ctx) {
+      ctx.height = 130;
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          type: 'line',
+          datasets: [{
+            data: [78, 81, 80, 45, 34, 12, 40],
+            label: 'Dataset',
+            backgroundColor: 'rgba(255,255,255,.1)',
+            borderColor: 'rgba(255,255,255,.55)',
+          },]
+        },
+        options: {
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+          responsive: true,
+          tooltips: {
+            mode: 'index',
+            titleFontSize: 12,
+            titleFontColor: '#000',
+            bodyFontColor: '#000',
+            backgroundColor: '#fff',
+            titleFontFamily: 'Montserrat',
+            bodyFontFamily: 'Montserrat',
+            cornerRadius: 3,
+            intersect: false,
+          },
+          scales: {
+            xAxes: [{
+              gridLines: {
+                color: 'transparent',
+                zeroLineColor: 'transparent'
+              },
+              ticks: {
+                fontSize: 2,
+                fontColor: 'transparent'
+              }
+            }],
+            yAxes: [{
+              display: false,
+              ticks: {
+                display: false,
+              }
+            }]
+          },
+          title: {
+            display: false,
+          },
+          elements: {
+            line: {
+              tension: 0.00001,
+              borderWidth: 1
+            },
+            point: {
+              radius: 4,
+              hitRadius: 10,
+              hoverRadius: 4
+            }
+          }
+        }
+      });
+    }
 
 
     //WidgetChart 2
-    var ctx = document.getElementById("widgetChart1");
-    var ctx2 = document.getElementById("widgetChart2");
+    var ctx = document.getElementById("widgetChart2");
     var design = {
       type: 'line',
       data: {
@@ -140,83 +143,47 @@
     if (ctx) {
       ctx.height = 130;
       var myChart = new Chart(ctx, design);
-      var myChart2 = new Chart(ctx2, design);
     }
 
 
     //WidgetChart 3
-    // var ctx = document.getElementById("widgetChart3");
-    // if (ctx) {
-    //   ctx.height = 130;
-    //   var myChart = new Chart(ctx, {
-    //     type: 'line',
-    //     data: {
-    //       labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    //       type: 'line',
-    //       datasets: [{
-    //         data: [65, 59, 84, 84, 51, 55],
-    //         label: 'Dataset',
-    //         backgroundColor: 'transparent',
-    //         borderColor: 'rgba(255,255,255,.55)',
-    //       },]
-    //     },
-    //     options: {
-    //
-    //       maintainAspectRatio: false,
-    //       legend: {
-    //         display: false
-    //       },
-    //       responsive: true,
-    //       tooltips: {
-    //         mode: 'index',
-    //         titleFontSize: 12,
-    //         titleFontColor: '#000',
-    //         bodyFontColor: '#000',
-    //         backgroundColor: '#fff',
-    //         titleFontFamily: 'Montserrat',
-    //         bodyFontFamily: 'Montserrat',
-    //         cornerRadius: 3,
-    //         intersect: false,
-    //       },
-    //       scales: {
-    //         xAxes: [{
-    //           gridLines: {
-    //             color: 'transparent',
-    //             zeroLineColor: 'transparent'
-    //           },
-    //           ticks: {
-    //             fontSize: 2,
-    //             fontColor: 'transparent'
-    //           }
-    //         }],
-    //         yAxes: [{
-    //           display: false,
-    //           ticks: {
-    //             display: false,
-    //           }
-    //         }]
-    //       },
-    //       title: {
-    //         display: false,
-    //       },
-    //       elements: {
-    //         line: {
-    //           borderWidth: 1
-    //         },
-    //         point: {
-    //           radius: 4,
-    //           hitRadius: 10,
-    //           hoverRadius: 4
-    //         }
-    //       }
-    //     }
-    //   });
-    // }
+    var ctx = document.getElementById("widgetChart3");
+    if (ctx) {
+      ctx.height = 130;
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+          type: 'line',
+          datasets: [{
+            data: [65, 59, 84, 84, 51, 55],
+            label: 'Dataset',
+            backgroundColor: 'transparent',
+            borderColor: 'rgba(255,255,255,.55)',
+          },]
+        },
+        options: {
+          maintainAspectRatio: true,
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: false,
+              categoryPercentage: 1,
+              barPercentage: 0.65
+            }],
+            yAxes: [{
+              display: false
+            }]
+          }
+        }
+      });
+    }
 
 
     //WidgetChart 4
-    var ctx = document.getElementById("widgetChart3");
-    var ctx2 = document.getElementById("widgetChart4");
+    var ctx = document.getElementById("widgetChart4");
     var design = {
       type: 'bar',
       data: {
@@ -251,7 +218,6 @@
     if (ctx) {
       ctx.height = 115;
       var myChart = new Chart(ctx, design);
-      var myChart2 = new Chart(ctx2, design);
     }
 
     // Recent Report
