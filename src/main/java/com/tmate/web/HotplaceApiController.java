fde491de03ab162287e1eeeb32e962d446e1a134
cherr0 <adsl1664@gmail.com>
2021-03-01 17:25:58 +0900

    api controller 삭제 및 분리

---
+++ src/main/java/com/tmate/web/HotplaceApiController.java
@@ -0,0 +1,12 @@
+package com.tmate.web;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class HotplaceApiController {
+}
