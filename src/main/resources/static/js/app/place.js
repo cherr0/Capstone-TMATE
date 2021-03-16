04ab3b9860d17454f07e1da04ade2e00159c4fc5
cherr0 <adsl1664@gmail.com>
2021-03-16 14:36:39 +0900

    핫플레이스 삭제 구현

--- src/main/resources/static/js/app/place.js
+++ src/main/resources/static/js/app/place.js
@@ -3,28 +3,33 @@ const place = {
     // 클릭 이벤트 연결
     init : function () {
         const _this = this;
+        console.log('place 클릭 이벤트 연결');
 
-        $('hotplace-remove').on('click',function () {
-           _this.placeRemove();
-        });
+
     },
 
     // 핫플레이스 삭제
-    placeRemove : function () {
-        const data = {
-            pl_id: $('#pl_id').val()
-        };
+    placeRemove : function (event) {
+        const pl_id = event.find('.infowindow_content').html();
 
         $.ajax({
-           data: JSON.stringify(data),
+           data: pl_id,
            type: 'DELETE',
-           url: '/api/hotplaceremove',
+           url: '/api/hotplaceremove/' + pl_id,
            contentType: 'application/json; charset=utf-8'
         }).done(function () {
-            alert('핫플레이스 삭제 완료');
-            window.location.href = '/hotplace';
+            swal({
+                title: "삭제 성공",
+                text: "핫플레이스 삭제가 완료되었습니다.",
+                icon: "success"
+            }).then(() => {
+                window.location.href = '/hotplace';
+            })
+
         }).fail(function (err) {
             alert(JSON.stringify(err));
         });
     }
 }
+
+place.init();
