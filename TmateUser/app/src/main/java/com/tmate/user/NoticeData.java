004cc1a14698b4a66d357595ebf41daa468cc33d
sihyeon955 <75560041+sihyeon955@users.noreply.github.com>
2021-03-28 16:10:11 +0900

    공지사항 리사이클러뷰 자바

---
+++ TmateUser/app/src/main/java/com/tmate/user/NoticeData.java
@@ -0,0 +1,22 @@
+package com.tmate.user;
+
+public class NoticeData {
+    private String notice_title;
+    private String notice_date;
+
+    public String getNotice_title() {
+        return notice_title;
+    }
+
+    public void setNotice_title(String notice_title) {
+        this.notice_title = notice_title;
+    }
+
+    public String getNotice_date() {
+        return notice_date;
+    }
+
+    public void setNotice_date(String notice_date) {
+        this.notice_date = notice_date;
+    }
+}
