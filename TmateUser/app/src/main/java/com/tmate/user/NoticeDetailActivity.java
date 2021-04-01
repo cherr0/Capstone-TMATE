ded3cd7b25a8f8d18a7778f08c216c6f9bbd9339
maple0904 <76030399+maple0904@users.noreply.github.com>
2021-04-01 23:49:52 +0900

    공지사항 <- 기능구현

--- TmateUser/app/src/main/java/com/tmate/user/NoticeDetailActivity.java
+++ TmateUser/app/src/main/java/com/tmate/user/NoticeDetailActivity.java
@@ -1,15 +1,27 @@
 package com.tmate.user;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NoticeDetailActivity extends AppCompatActivity {
+
+    private ImageView btn_back_noticeDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_detail);
 
+        btn_back_noticeDetail = findViewById(R.id.btn_back_noticeDetail);
+        btn_back_noticeDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
