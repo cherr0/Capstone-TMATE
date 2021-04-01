aec6579a491efca96f1d2c62d90fbf1deea36cf8
maple0904 <76030399+maple0904@users.noreply.github.com>
2021-04-01 23:48:56 +0900

    이벤트 <-키 기능 구현

--- TmateUser/app/src/main/java/com/tmate/user/EventDetailActivity.java
+++ TmateUser/app/src/main/java/com/tmate/user/EventDetailActivity.java
@@ -1,15 +1,26 @@
 package com.tmate.user;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventDetailActivity extends AppCompatActivity {
+
+    private ImageView btn_back_eventDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
 
+        btn_back_eventDetail = findViewById(R.id.btn_back_eventDetail);
+        btn_back_eventDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
