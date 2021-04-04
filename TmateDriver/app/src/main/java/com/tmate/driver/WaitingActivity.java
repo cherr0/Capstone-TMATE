8bea3d81fdc0abacfb41760f2e54f12eeda195ea
hansu6612 <lsr9543@naver.com>
2021-04-04 21:29:32 +0900

    기사 매칭 및 결제 뷰 제작 완료

--- TmateDriver/app/src/main/java/com/tmate/driver/WaitingActivity.java
+++ TmateDriver/app/src/main/java/com/tmate/driver/WaitingActivity.java
@@ -2,7 +2,9 @@ package com.tmate.driver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,5 +23,13 @@ public class WaitingActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WaitingActivity.this, MatchingActivity.class);
+                startActivity(intent);
+            }
+        }, 4000);
     }
 }
