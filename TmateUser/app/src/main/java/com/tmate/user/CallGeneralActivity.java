580f7e710916f37478381a8f8d3379908970e372
wonjun <77709865+jangwonjun1@users.noreply.github.com>
2021-04-07 16:02:17 +0900

    결제정보확인 Activity 화면전환 코드작성

--- TmateUser/app/src/main/java/com/tmate/user/CallGeneralActivity.java
+++ TmateUser/app/src/main/java/com/tmate/user/CallGeneralActivity.java
@@ -2,13 +2,29 @@ package com.tmate.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CallGeneralActivity extends AppCompatActivity {
+
+    Button btn_fare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_general);
+
+        btn_fare = findViewById(R.id.btn_fare);
+
+        btn_fare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CallGeneralActivity.this,PaymentSuccessActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
