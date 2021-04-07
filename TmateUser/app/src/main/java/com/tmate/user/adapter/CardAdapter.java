34d77aa5f783c44c11b458e189d7c37d279873eb
hansu6612 <hansu6612@g.yju.ac.kr>
2021-04-07 19:13:37 +0900

    카드관리 디자인 수정 및 앱 아이콘 변경 완료

--- TmateUser/app/src/main/java/com/tmate/user/adapter/CardAdapter.java
+++ TmateUser/app/src/main/java/com/tmate/user/adapter/CardAdapter.java
@@ -1,5 +1,4 @@
 package com.tmate.user.adapter;
-
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,37 +6,24 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
-
-
 import com.tmate.user.R;
 import com.tmate.user.data.CardData;
-
 import java.util.ArrayList;
-
 public class CardAdapter extends  RecyclerView.Adapter<CardHolder>{
-
     ArrayList<CardData> items = new ArrayList<>();
-
     public int r = 0;
-
-
-
     @NonNull
     @Override
     public CardHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_list, parent, false);
         return new CardHolder(view);
     }
-
-
     @Override
     public void onBindViewHolder(@NonNull CardHolder holder, int position) {
         holder.onBind(items.get(position));
-
         holder.cardDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,7 +36,6 @@ public class CardAdapter extends  RecyclerView.Adapter<CardHolder>{
                                 items.remove(position);
                                 notifyItemRemoved(position);
                                 notifyItemRangeChanged(position, items.size());
-
                             }
                         });
                 builder.setNegativeButton("아니오",
@@ -60,43 +45,32 @@ public class CardAdapter extends  RecyclerView.Adapter<CardHolder>{
                             }
                         });
                 builder.show();
-
             }
         });
     }
-
-
     @Override
     public int getItemCount() {
         return items.size();
     }
-
-
-
     public void addItem(CardData data) {
         items.add(data);
     }
-
-
-
 }
-
 class CardHolder extends RecyclerView.ViewHolder {
     ImageView bank_mark;
     TextView bank_name;
+    TextView card_no;
     Button cardDelete;
-
     void onBind(CardData data) {
         bank_mark.setImageResource(data.getCardmark());
         bank_name.setText(data.getBankName());
-
+        card_no.setText(data.getCardNo());
     }
-
     public CardHolder(@NonNull View itemView) {
         super(itemView);
         bank_mark = itemView.findViewById(R.id.bank_mark);
         bank_name= itemView.findViewById(R.id.bank_name);
         cardDelete = itemView.findViewById(R.id.deleteCard);
+        card_no = itemView.findViewById(R.id.card_no);
     }
 }
-
--- TmateUser/app/src/main/java/com/tmate/user/data/CardData.java
+++ TmateUser/app/src/main/java/com/tmate/user/data/CardData.java
@@ -1,30 +1,30 @@
 package com.tmate.user.data;
-
 public class CardData {
     private int cardmark;
     private String bankName;
     private String carddelete;
-
+    private String cardNo;
+    public String getCardNo() {
+        return cardNo;
+    }
+    public void setCardNo(String cardNo) {
+        this.cardNo = cardNo;
+    }
     public int getCardmark() {
         return cardmark;
     }
-
     public void setCardmark(int cardmark) {
         this.cardmark = cardmark;
     }
-
     public String getBankName() {
         return bankName;
     }
-
     public void setBankName(String bankName) {
         this.bankName = bankName;
     }
-
     public String getCarddelete() {
         return carddelete;
     }
-
     public void setCarddelete(String carddelete) {
         this.carddelete = carddelete;
     }
