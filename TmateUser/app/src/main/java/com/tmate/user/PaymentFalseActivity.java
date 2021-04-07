f9d1fabe798f88e705ad15f229f141eb9d1f1c8d
wonjun <77709865+jangwonjun1@users.noreply.github.com>
2021-04-07 16:00:31 +0900

    결제 실패 뷰, 자바 제작

---
+++ TmateUser/app/src/main/java/com/tmate/user/PaymentFalseActivity.java
@@ -0,0 +1,14 @@
+package com.tmate.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentFalseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_false);
+    }
+}
