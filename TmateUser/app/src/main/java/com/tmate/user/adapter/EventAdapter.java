d581634e265bae7814866d717f1c201ea055928c
cherr0 <adsl1664@gmail.com>
2021-04-11 14:14:40 +0900

    EventAdapter Holder 분리

--- TmateUser/app/src/main/java/com/tmate/user/adapter/EventAdapter.java
+++ TmateUser/app/src/main/java/com/tmate/user/adapter/EventAdapter.java
@@ -22,7 +22,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class EventAdapter extends RecyclerView.Adapter<EventAdapter.CustomViewHolder> {
+public class EventAdapter extends RecyclerView.Adapter<EventHolder> {
 
     private ArrayList<EventData> arrayList;
 
@@ -34,15 +34,14 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.CustomViewHo
 
     @NonNull
     @Override
-    public EventAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public EventHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_event_item, parent, false);
-        EventAdapter.CustomViewHolder holder = new EventAdapter.CustomViewHolder(view);
-        return holder;
+        return new EventHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull EventAdapter.CustomViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull EventHolder holder, int position) {
         holder.iv_event.setImageResource(arrayList.get(position).getIv_event());
         holder.tv_title.setText(arrayList.get(position).getTv_title());
         holder.tv_subTitle.setText(arrayList.get(position).getTv_subTitle());
@@ -97,23 +96,23 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.CustomViewHo
     public void addItem(EventAdapter eventAdapter) {
     }
 
-    public class CustomViewHolder extends RecyclerView.ViewHolder {
+}
 
-        protected ImageView iv_event;
-        protected TextView tv_title;
-        protected TextView tv_date;
-        protected TextView tv_subTitle;
-        protected TextView tv_bd_id;
+class EventHolder extends RecyclerView.ViewHolder {
 
+    ImageView iv_event;
+    TextView tv_title;
+    TextView tv_date;
+    TextView tv_subTitle;
+    TextView tv_bd_id;
 
-        public CustomViewHolder(View itemView) {
-            super(itemView);
+    public EventHolder(View itemView){
+        super(itemView);
 
-            this.iv_event = (ImageView) itemView.findViewById(R.id.iv_event);
-            this.tv_title = (TextView) itemView.findViewById(R.id.tv_title);
-            this.tv_subTitle = (TextView) itemView.findViewById(R.id.tv_subTitle);
-            this.tv_date = (TextView) itemView.findViewById(R.id.tv_date);
-            this.tv_bd_id = (TextView) itemView.findViewById(R.id.tv_bd_id);
-        }
+        this.iv_event = (ImageView) itemView.findViewById(R.id.iv_event);
+        this.tv_title = (TextView) itemView.findViewById(R.id.tv_title);
+        this.tv_subTitle = (TextView) itemView.findViewById(R.id.tv_subTitle);
+        this.tv_date = (TextView) itemView.findViewById(R.id.tv_date);
+        this.tv_bd_id = (TextView) itemView.findViewById(R.id.tv_bd_id);
     }
 }
