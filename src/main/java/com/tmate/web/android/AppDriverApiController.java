1fa71d19c49cae28225e0ed1eb88e5aad52017df
cherr0 <adsl1664@gmail.com>
2021-04-13 18:50:19 +0900

    기사앱 컨트롤러 리스트 작성

---
+++ src/main/java/com/tmate/web/android/AppDriverApiController.java
@@ -0,0 +1,33 @@
+package com.tmate.web.android;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/driver")
+@RequiredArgsConstructor
+@Log4j2
+public class AppDriverApiController {
+
+    // 기사 회원가입 - POST
+
+    // 기사 상태 바꾸기 - PUT
+
+    // 운행 기록 리스트 - GET
+
+    // 기사 평점 확인 리스트 - GET
+
+    // 기사 차량 리스트 - GET
+
+    // 블랙리스트 확인 - GET
+
+    // 블랙리스트 추가 - POST
+
+}
