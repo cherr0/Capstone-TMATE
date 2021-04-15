d2595a4785e44bd3df2dbeba39b6d2914478ac04
cherr0 <adsl1664@gmail.com>
2021-04-16 01:42:22 +0900

    기사 프로필 VO 생성

---
+++ src/main/java/com/tmate/domain/driver/DriverProfileVO.java
@@ -0,0 +1,46 @@
+package com.tmate.domain.driver;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverProfileVO {
+    /* 사용처 :
+        기사 프로필 정보 가져오기
+     */
+
+    // 회원 이름
+    private String m_name;
+
+    // 휴대폰 번호
+    private String phone;
+
+    // 이메일
+    private String m_email;
+
+    // 은행사
+    private String bank_company;
+
+    // 계좌번호
+    private String d_acnum;
+
+    // 기사 등록 일자
+    private Timestamp d_j_date;
+
+    // 일반 운행 수
+    private int no_cnt;
+
+    // 동승 운행 수
+    private int to_cnt;
+
+    // 기사 총 수입 (함수 사용)
+    private int all_fare;
+
+    // 기사 이 달 수입 (함수 사용)
+    private int month_fare;
+}
