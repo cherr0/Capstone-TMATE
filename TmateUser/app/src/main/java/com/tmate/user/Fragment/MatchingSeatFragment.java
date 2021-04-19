09c31a2a4ec24c029d3cccc1d40a9ae1885d9986
sihyeon955 <75560041+sihyeon955@users.noreply.github.com>
2021-04-19 19:00:09 +0900

    유저앱 자리 하나만 선택

--- TmateUser/app/src/main/java/com/tmate/user/Fragment/MatchingSeatFragment.java
+++ TmateUser/app/src/main/java/com/tmate/user/Fragment/MatchingSeatFragment.java
@@ -14,42 +14,71 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.tmate.user.Activity.CallGeneralActivity;
+import com.tmate.user.Activity.MatchingActivity;
 import com.tmate.user.Activity.MatchingMapActivity;
 import com.tmate.user.R;
+import com.tmate.user.databinding.FragmentMatchingSeatBinding;
 
 
 public class MatchingSeatFragment extends Fragment {
     private View view;
-    Button btnSeat;
     private int togetherAdd = 1;
+    private FragmentMatchingSeatBinding b;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_matching_seat, container, false);
+        b = FragmentMatchingSeatBinding.inflate(getLayoutInflater());
+        view = b.getRoot();
 
         Bundle bundle = getArguments();  //만들고 있는 방의 좌석 체크인지 이미 만든 방의 좌석체크인지 확인
         if(bundle != null){
             togetherAdd = bundle.getInt("togetherAdd"); //Name 받기.
             System.out.println(togetherAdd); //확인
         }
-
-        btnSeat = view.findViewById(R.id.btn_seat);
-        btnSeat.setOnClickListener(new View.OnClickListener() {
+        //자리 선택 완료 후 버튼클릭이벤트
+        b.btnSeat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(togetherAdd == 0) {
+                if(togetherAdd == 0) {        //매칭방 만들때의 경로(자리 선택 후 매칭리스트로 이동)
                     FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
                     MatchingFragment mf = new MatchingFragment();
                     transaction.replace(R.id.fm_matching_activity, mf);
                     transaction.commit();
-                }
-                else {
+                } else { //이미 만들어진 매칭방에서 들어왔을때의 경로(결제화면으로 이동)
                     Intent intent = new Intent(getContext(),CallGeneralActivity.class);
                     startActivity(intent);
                 }
             }
         });
+        //자리 1번을 선택할 경우
+        b.seatOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.seatOne.setChecked(true);
+                b.seatTwo.setChecked(false);
+                b.seatThree.setChecked(false);
+            }
+        });
+        //자리 2를 선택할 경우
+        b.seatTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.seatTwo.setChecked(true);
+                b.seatOne.setChecked(false);
+                b.seatThree.setChecked(false);
+            }
+        });
+        //자리 3을 선택할 경우
+        b.seatThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.seatThree.setChecked(true);
+                b.seatTwo.setChecked(false);
+                b.seatOne.setChecked(false);
+            }
+        });
         return view;
     }
+
 }
