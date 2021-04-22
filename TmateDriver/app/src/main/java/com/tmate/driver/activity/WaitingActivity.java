7bbe98cf53733638dce5225d085823808d62cdbb
hansu6612 <hansu6612@g.yju.ac.kr>
2021-04-22 19:05:53 +0900

    기사 매칭 다이얼로그 제작 완료

--- TmateDriver/app/src/main/java/com/tmate/driver/activity/WaitingActivity.java
+++ TmateDriver/app/src/main/java/com/tmate/driver/activity/WaitingActivity.java
@@ -1,9 +1,11 @@
 package com.tmate.driver.activity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,18 @@ import com.tmate.driver.R;
 
 public class WaitingActivity extends AppCompatActivity {
     Button btn_drive_stop;
+    private Dialog dialog;
+    private Button matching_btn_refusal;
+    private Button matching_btn_accept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waiting);
+
+        dialog = new Dialog(WaitingActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_matching);
 
         btn_drive_stop = findViewById(R.id.btn_drive_stop);
         btn_drive_stop.setOnClickListener(new View.OnClickListener() {
@@ -29,9 +38,28 @@ public class WaitingActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(WaitingActivity.this, MatchingActivity.class);
-                startActivity(intent);
+                showDialog();
             }
         }, 4000);
+    }
+
+    public void showDialog(){
+        dialog.show();
+
+        matching_btn_refusal = dialog.findViewById(R.id.matching_btn_refusal);
+        matching_btn_refusal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        matching_btn_accept = dialog.findViewById(R.id.matching_btn_accept);
+        matching_btn_accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WaitingActivity.this, PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
