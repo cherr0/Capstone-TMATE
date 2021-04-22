384e0f9638e87492852e999579fa86f0f0e1416e
cherr0 <adsl1664@gmail.com>
2021-04-22 15:10:11 +0900

    도메인 객체 추가
    Event, Notice 객체 추가

---
+++ TmateDriver/app/src/main/java/com/tmate/driver/data/Notice.java
@@ -0,0 +1,37 @@
+package com.tmate.driver.data;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notice {
+
+    // 게시판 코드
+    private String bd_id;
+
+    // 게시판 상태
+    private String bd_status;
+
+    // 게시판 내용
+    private String bd_contents;
+
+    // 게시판 제목
+    private String bd_title;
+
+    // 조회수
+    private int bd_count;
+
+    // 작성일자
+    private Timestamp bd_cre_date;
+
+    // 수정 일자
+    private Timestamp bd_mod_date;
+
+    // 작성자 회원 코드
+    private String m_id;
+}
