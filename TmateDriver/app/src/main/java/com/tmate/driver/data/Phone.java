4f1ab2b6dd3d51533dd296fbaf12a491d1dc35c5
cherr0 <adsl1664@gmail.com>
2021-04-28 20:20:51 +0900

    Phone 데이터 추가

---
+++ TmateDriver/app/src/main/java/com/tmate/driver/data/Phone.java
@@ -0,0 +1,20 @@
+package com.tmate.driver.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    // 이름
+    private String name;
+
+    // 휴대폰 번호
+    private String phoneNumber;
+
+    // 인증 번호
+    private String confirm;
+}
