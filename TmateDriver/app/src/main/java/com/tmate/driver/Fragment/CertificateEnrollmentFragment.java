75d4b2c148f663359a09110c755309bd1bd3b6db
cherr0 <adsl1664@gmail.com>
2021-04-29 19:19:03 +0900

    자격증 인증 유효성검사 추가

--- TmateDriver/app/src/main/java/com/tmate/driver/Fragment/CertificateEnrollmentFragment.java
+++ TmateDriver/app/src/main/java/com/tmate/driver/Fragment/CertificateEnrollmentFragment.java
@@ -12,9 +12,11 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,32 +28,52 @@ import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Length;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.tmate.driver.R;
 import com.tmate.driver.databinding.FragmentCertificateEnrollmentBinding;
 
 import java.io.File;
+import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
 
-public class CertificateEnrollmentFragment extends Fragment {
+public class CertificateEnrollmentFragment extends Fragment implements Validator.ValidationListener {
     private FragmentCertificateEnrollmentBinding b;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public final static int REQUEST_PERMISSIONS_REQUEST_CODE = 100;
 
+    Bundle bundle;
+
+    @Length(min = 12, max = 12, message = "'-' 까지 명시하여 정확히 입력바랍니다.")
+    @NotEmpty(message = "자격증 번호를 입력 바랍니다.")
+    EditText license_no;
+
+    Validator validator;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         b = FragmentCertificateEnrollmentBinding.inflate(inflater, container, false);
         View view = b.getRoot();
+
+        if (getArguments() != null) {
+            bundle = getArguments();
+            Log.d("번들 넘어오는 값",bundle.toString());
+        }else {
+            Log.d("CEFragment.Bundle","번들 값을 받아오지 못했습니다.");
+        }
+
+        license_no = view.findViewById(R.id.license_no);
+        validator = new Validator(this);//필수
+        validator.setValidationListener(this); //필수
+
         b.btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                AccountRegistrationFragment arf = new AccountRegistrationFragment();
-                transaction.replace(R.id.fm_main, arf);
-                transaction.addToBackStack(null);
-                transaction.commit();
+                validator.validate(); //버튼 클릭 시 이벤트 발생 //필수
             }
         });
         b.licenseIma.setOnClickListener(new View.OnClickListener() {
@@ -153,6 +175,32 @@ public class CertificateEnrollmentFragment extends Fragment {
                     b.licenseIma.setImageURI(selectedImage);
                 }
                 break;
+        }
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        bundle.putString("d_license_no", license_no.getText().toString());
+        Log.i("자격증 번호", "값 저장됨 : " + license_no.getText().toString());
+
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        AccountRegistrationFragment arf = new AccountRegistrationFragment();
+        arf.setArguments(bundle);
+        transaction.replace(R.id.fm_main, arf);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(getActivity());
+            if (view instanceof EditText) {
+                ((EditText)view).setError(message);
+            } else {
+                Toast.makeText(getActivity(),message,Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
