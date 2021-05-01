863a5c6a9df54ce9dda2dc640a34dc643307dba5
cherr0 <adsl1664@gmail.com>
2021-05-01 12:28:38 +0900

    회원 가입 시 이름 Shared에 추가

--- TmateDriver/app/src/main/java/com/tmate/driver/Fragment/AccountRegistrationFragment.java
+++ TmateDriver/app/src/main/java/com/tmate/driver/Fragment/AccountRegistrationFragment.java
@@ -137,6 +137,7 @@ public class AccountRegistrationFragment extends Fragment implements Validator.V
                 if(response.code() == 200) {
                     Log.i("AccountRegFragment","가입 신청 완료");
                     setPreference("d_id",bundle.getString("m_id"));
+                    setPreference("m_name",bundle.getString("m_name"));
                     FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
                     CompletedFragment cf = new CompletedFragment();
                     cf.setArguments(bundle);
