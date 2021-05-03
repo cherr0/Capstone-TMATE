6e7529f2f319f81535cfc862a5a21733d94f9734
maple0904 <76030399+maple0904@users.noreply.github.com>
2021-05-03 20:38:18 +0900

    사용자앱 매칭 신청 목록 제작

---
+++ TmateUser/app/src/main/java/com/tmate/user/data/MatchingApplicationList.java
@@ -0,0 +1,67 @@
+package com.tmate.user.data;
+
+public class MatchingApplicationList {
+    private int application_m_profile;
+    private String application_m_name;
+    private String application_h_s_place;
+    private String application_h_f_place;
+    private String application_read;
+    private String application_personnel;
+    private String application_merchant_uid;
+
+    public int getApplication_m_profile() {
+        return application_m_profile;
+    }
+
+    public void setApplication_m_profile(int application_m_profile) {
+        this.application_m_profile = application_m_profile;
+    }
+
+    public String getApplication_m_name() {
+        return application_m_name;
+    }
+
+    public void setApplication_m_name(String application_m_name) {
+        this.application_m_name = application_m_name;
+    }
+
+    public String getApplication_h_s_place() {
+        return application_h_s_place;
+    }
+
+    public void setApplication_h_s_place(String application_h_s_place) {
+        this.application_h_s_place = application_h_s_place;
+    }
+
+    public String getApplication_h_f_place() {
+        return application_h_f_place;
+    }
+
+    public void setApplication_h_f_place(String application_h_f_place) {
+        this.application_h_f_place = application_h_f_place;
+    }
+
+    public String getApplication_read() {
+        return application_read;
+    }
+
+    public void setApplication_read(String application_read) {
+        this.application_read = application_read;
+    }
+
+    public String getApplication_personnel() {
+        return application_personnel;
+    }
+
+    public void setApplication_personnel(String application_personnel) {
+        this.application_personnel = application_personnel;
+    }
+
+    public String getApplication_merchant_uid() {
+        return application_merchant_uid;
+    }
+
+    public void setApplication_merchant_uid(String application_merchant_uid) {
+        this.application_merchant_uid = application_merchant_uid;
+    }
+}
