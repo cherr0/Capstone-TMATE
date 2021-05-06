227d09551110e51135e76efde40703a9cb9bb93e
hazzang777 <68279162+hazzang777@users.noreply.github.com>
2021-05-06 15:05:45 +0900

    URL변경

--- TmateUser/app/src/main/java/com/tmate/user/net/DataService.java
+++ TmateUser/app/src/main/java/com/tmate/user/net/DataService.java
@@ -18,7 +18,7 @@ public class DataService {
     * */
 
     // EC2 서버 URL
-     private static final String BASE_URL = "http://ec2-52-79-142-104.ap-northeast-2.compute.amazonaws.com:8080/common/"; // 기본 URL
+     private static final String BASE_URL = "http://ec2-52-79-142-104.ap-northeast-2.compute.amazonaws.com:8080/"; // 기본 URL
 
     // 안드로이드 폰 연결
 //    private static final String BASE_URL = "http://172.26.3.112:9090/";
