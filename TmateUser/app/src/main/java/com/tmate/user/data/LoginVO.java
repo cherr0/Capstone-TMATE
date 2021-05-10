5f22226744f97fa891e5830b534bdf06eaa6bd0e
cherr0 <adsl1664@gmail.com>
2021-05-10 20:25:50 +0900

    Data 객체 추가

---
+++ TmateUser/app/src/main/java/com/tmate/user/data/LoginVO.java
@@ -0,0 +1,29 @@
+package com.tmate.user.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginVO {
+
+    // 이메일 또는 휴대폰번호
+    private String id;
+
+    // 비밀번호
+    private String password;
+
+    // 권한 확인
+    private String auth;
+
+    // 회원코드
+    private String m_id;
+
+    // 회원 이름
+    private String m_name;
+
+    // Imei 값
+    private String m_imei;
+}
