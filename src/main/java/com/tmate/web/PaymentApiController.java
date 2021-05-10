528770b1c65fe71eea6977a4e34740e288df6e45
cherr0 <adsl1664@gmail.com>
2021-05-10 15:11:05 +0900

    리턴 값 변경

--- src/main/java/com/tmate/web/PaymentApiController.java
+++ src/main/java/com/tmate/web/PaymentApiController.java
@@ -4,6 +4,7 @@ import com.tmate.domain.KakaoDTO;
 import com.tmate.service.android.user.AppPaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,16 +20,16 @@ public class PaymentApiController {
     private final AppPaymentService appPaymentService;
 
     @PostMapping("/kakaoReady")
-    public Boolean kakaoReady(@RequestBody Map<String, String> map) {
+    public ResponseEntity<Boolean> kakaoReady(@RequestBody Map<String, String> map) {
         log.info("안드로이드에서 넘어온 결제 준비 정보 : " + map);
 
-        return appPaymentService.kakaoReady(map);
+        return new ResponseEntity<>(appPaymentService.kakaoReady(map), HttpStatus.OK);
     }
 
     @PutMapping("/kakaoApprove")
-    public Boolean kakaoApprove(@RequestBody KakaoDTO kakaoDTO) {
+    public ResponseEntity<Boolean> kakaoApprove(@RequestBody KakaoDTO kakaoDTO) {
         log.info("거래 승인 완료 정보 : " + kakaoDTO);
 
-        return appPaymentService.kakaoApprove(kakaoDTO);
+        return new ResponseEntity<>(appPaymentService.kakaoApprove(kakaoDTO), HttpStatus.OK);
     }
 }
