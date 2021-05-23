f5525455cdbe2b397ff85b87331834abce0dc9ad
cherr0 <adsl1664@gmail.com>
2021-05-23 17:56:40 +0900

    DrivingModel 필요없는 파일 삭제

--- TmateUser/app/src/main/java/com/tmate/user/ui/driving/DrivingModel.java
+++ TmateUser/app/src/main/java/com/tmate/user/ui/driving/DrivingModel.java
@@ -18,21 +18,9 @@ public class DrivingModel extends ViewModel {
     public static final String mApiKey = "l7xx4fac78a5b9bf445db00bb99ae2708cee";
 
     public int together = 0; //동승 설정
-    public int togetherOption;//동승 몇명인지
 
     //DB에 들어갈 거리와 시간에 대한 변수들
     public Dispatch dispatch;
 
     public MutableLiveData<String> start = new MutableLiveData<>();
-
-
-    //출발지 도착지에 대한 변수들
-    public TMapPoint tMapPointStart;//출발지 위,경도를 담은 좌표
-    public TMapPoint tMapPointEnd;//도착지 위,경도를 담은 좌표
-
-    //마커에 대한 변수들
-    public ArrayList<TMapPOIItem> mArrPoiItem; // 검색값에 대한 리스트
-    public CharSequence[] items;
-
-    public int m_nCurrentZoomLevel = 0;//줌 레벨에 대한 변수
 }
