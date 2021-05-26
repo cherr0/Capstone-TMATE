b67928774d3b87edd74bd7d56a969cdd33af0f76
sihyeon955 <75560041+sihyeon955@users.noreply.github.com>
2021-05-26 15:26:43 +0900

    유저앱 평가하기 창 수정

--- TmateUser/app/src/main/java/com/tmate/user/adapter/ReviewAdapter.java
+++ TmateUser/app/src/main/java/com/tmate/user/adapter/ReviewAdapter.java
@@ -7,13 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.common.internal.GmsClientEventManager;
 import com.tmate.user.R;
 import com.tmate.user.data.ReviewData;
 
@@ -40,107 +43,30 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewHolder>{
     public void onBindViewHolder(@NonNull ReviewHolder holder, int position) {
         holder.onBind(items.get(position));
 
-        holder.btn_review.setOnClickListener(new View.OnClickListener() {
+        holder.member_like.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                dialog = new Dialog(holder.btn_review.getContext());
-                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                dialog.setContentView(R.layout.dialog_review);
-                dialog.show();
-                ImageView good = dialog.findViewById(R.id.r_good);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                holder.member_dislike.setChecked(false); //싫어요 off
+                if(isChecked) {
+                    holder.member_like.setChecked(true);
+                } else if(!isChecked){
+                    holder.member_like.setChecked(false);
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(holder.btn_review.getContext());
-                final ArrayList<String> selectedItems= new ArrayList<String>();
-                TextView titleMName = dialog.findViewById(R.id.title_m_name);
-                titleMName.setText(holder.mi_m_name.getText());
-                TextView contentMName = dialog.findViewById(R.id.content_m_name);
-                contentMName.setText(holder.mi_m_name.getText());
-                good.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                        final String[] result = holder.btn_review.getContext().getResources().getStringArray(R.array.member_like_review);
-                        builder.setTitle("좋아요 한 사유를 선택해주세요");
-                        builder.setMultiChoiceItems(R.array.member_like_review, null, new DialogInterface.OnMultiChoiceClickListener(){
-                            @Override
-                            public void onClick(DialogInterface dialog, int pos, boolean isChecked)
-                            {
-                                if(isChecked == true) // Checked 상태일 때 추가
-                                {
-                                    try {
-                                        selectedItems.add(result[pos]);
-                                    } catch (IndexOutOfBoundsException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                                else
-                                {
-                                    try {
-                                        selectedItems.remove(result[pos]);
-                                    } catch (IndexOutOfBoundsException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                        });
-                        builder.setPositiveButton("완료", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                if(selectedItems != null && !selectedItems.isEmpty()) {
-                                    holder.review_finish.setImageResource(R.drawable.review_checked);
-                                }
-                            }
-                        });
-
-                        AlertDialog alertDialog = builder.create();
-                        alertDialog.show();
-                    }
-                });
-                ImageView bad = dialog.findViewById(R.id.r_bad);
-                bad.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                        final String[] result = holder.btn_review.getContext().getResources().getStringArray(R.array.member_bad_review);
-                        builder.setTitle("싫어요 한 사유를 선택해주세요");
-                        builder.setMultiChoiceItems(R.array.member_bad_review, null, new DialogInterface.OnMultiChoiceClickListener(){
-                            @Override
-                            public void onClick(DialogInterface dialog, int pos, boolean isChecked)
-                            {
-                                if(isChecked == true) // Checked 상태일 때 추가
-                                {
-                                    try {
-                                        selectedItems.add(result[pos]);
-                                    } catch (IndexOutOfBoundsException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                                else
-                                {
-                                    try {
-                                        selectedItems.remove(result[pos]);
-                                    } catch (IndexOutOfBoundsException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                        });
-                        builder.setPositiveButton("완료", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                if(selectedItems != null && !selectedItems.isEmpty()) {
-                                    holder.review_finish.setImageResource(R.drawable.review_checked);
-                                }
-                            }
-                        });
-
-                        AlertDialog alertDialog = builder.create();
-                        alertDialog.show();
-                    }
-                });
-
+                }
             }
         });
+        holder.member_dislike.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                holder.member_like.setChecked(false); //좋아요 off
+                if(isChecked) {
+                    holder.member_dislike.setChecked(true);
+                } else if (!isChecked){
+                    holder.member_dislike.setChecked(false);
+                }
+            }
+        });
+
 
     }
 
@@ -158,7 +84,8 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewHolder>{
 class ReviewHolder extends RecyclerView.ViewHolder {
     TextView mi_m_name;
     CircleImageView review_picture;
-    Button btn_review;
+    ToggleButton member_like;
+    ToggleButton member_dislike;
     ImageView review_finish;
     TextView r_to_seat;
 
@@ -167,7 +94,8 @@ class ReviewHolder extends RecyclerView.ViewHolder {
         mi_m_name = itemView.findViewById(R.id.review_m_name);
         review_finish = itemView.findViewById(R.id.review_finish);
         review_picture = itemView.findViewById(R.id.review_picture);
-        btn_review = itemView.findViewById(R.id.btn_review);
+        member_like = itemView.findViewById(R.id.member_like);
+        member_dislike = itemView.findViewById(R.id.member_dislike);
         r_to_seat = itemView.findViewById(R.id.r_to_seat);
     }
     void onBind(ReviewData data) {
