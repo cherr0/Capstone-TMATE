3f782048dcb0e3e8a61630cd8c2aa677d8556c4b
cherr0 <adsl1664@gmail.com>
2021-06-02 11:49:23 +0900

    dispatch에 값 넣도록 수정

--- TmateUser/app/src/main/java/com/tmate/user/ui/driving/DrivingActivity.java
+++ TmateUser/app/src/main/java/com/tmate/user/ui/driving/DrivingActivity.java
@@ -30,11 +30,13 @@ public class DrivingActivity extends AppCompatActivity {
         String at_status = intent.getStringExtra("at_status");
         String dp_id = intent.getStringExtra("dp_id");
         Log.d("DrivingActivity", "동승 인원 : " + together);
-        Log.d("DrivingActivity","배차 코드 : " + dp_id);
+        if(dp_id != null) Log.d("DrivingActivity","배차 코드 : " + dp_id);
+        if(at_status != null) Log.d("DrivingActivity", "배차 상태 : " + dp_status);
+        if(dp_status != null) Log.d("DrivingActivity", "참여 상태 : " + at_status);
 
         model.dispatch = new Dispatch(); // 객체에 값을 담을 수 있도록 액티비티 생성 시 생성
-        model.dp_status = dp_status;
-        model.at_status = at_status;
+        model.dispatch.setDp_status(dp_status);
+        model.dispatch.setAt_status(at_status);
         model.dispatch.setDp_id(dp_id);
         model.dispatch.setM_id(getPreferenceString("m_id")); // 시작 시 m_id 값 담기
 
