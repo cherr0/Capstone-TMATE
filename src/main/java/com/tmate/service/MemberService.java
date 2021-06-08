30e48d6f9b2fc7b7adddcb3d882643ec8a2fa080
cherr0 <adsl1664@gmail.com>
2021-06-08 11:35:24 +0900

    운행이력 관련 서비스 변경

--- src/main/java/com/tmate/service/MemberService.java
+++ src/main/java/com/tmate/service/MemberService.java
@@ -15,9 +15,6 @@ public interface MemberService {
     // 회원 눌렀을때 나오는 상세페이지 - 회원정보
     public MemberDTO getMember(String m_id);
 
-    // 회원 눌렀을때 나오는 상세페이지 - 이용내역
-    public List<JoinHistoryVO> getHistoryList(Criteria cri, String m_id);
-
     // 페이지 처리용 이용내역
     public HistoryPageDTO getListPage(Criteria cri, String m_id);
 
@@ -26,7 +23,6 @@ public interface MemberService {
 
     // 회원 눌렀을때 나오는 상세페이지 - 포인트 내역
     public List<JoinPointVO> getPointList(Criteria cri, String m_id);
-
 
     // 회원 리스트 부분 토탈 카운트
     public int getTotalCount(Criteria cri);
